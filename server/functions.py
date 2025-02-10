a87f61335c1c4e5653348289eebd4ce93908fdd6
Alexander Gomez <alexandergmzx@gmail.com>
2025-02-10 06:36:00 -0600

    Update for Pi Zero 2W and Ubuntu Server, add debugging guide

--- server/functions.py
+++ server/functions.py
@@ -2,7 +2,10 @@ import time
 import RPi.GPIO as GPIO
 import threading
 from mpu6050 import mpu6050
-import Adafruit_PCA9685
+## Import the CircuitPython PCA9685 module.
+from adafruit_pca9685 import PCA9685
+from board import SCL, SDA
+import busio
 import os
 import json
 import ultra
@@ -13,8 +16,11 @@ move.setup()
 
 kalman_filter_X =  Kalman_filter.Kalman_filter(0.01,0.1)
 
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(50)
+# Create the I2C bus interface.
+i2c = busio.I2C(SCL, SDA)
+# Create a simple PCA9685 class instance.
+pwm = PCA9685(i2c)
+pwm.frequency = 50
 
 MPU_connection = 1
 try:
